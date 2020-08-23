9365f048d9e1c48cda9ebf02d8221754be2301cf
Никита <nikitostokarev22@gmail.com>
2020-08-23 23:24:56 +0300

    add hide function

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
 $.getJSON("default.json", function (data) {
   $.each(data, function (key, data) {
-    $(".new-table").append(
-      `<tr>
+    if (data.parentId === 0) {
+      $(".new-table").append(
+        `<tr id="${data.id}" class="100">
         <th>${data.name}</th>
         <th>${data.balance}</th>       
         <th>${data.email}</th>
       </tr>`
-    );
+      );
+    }
   });
+  $('.100').click( function() {
+    $(this).hide()
+});
 });
