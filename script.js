1fea247b9f6a9266e764cc0b8d08d088fbfe7733
Никита <nikitostokarev22@gmail.com>
2020-08-23 22:30:49 +0300

    change map function

--- script.js
+++ script.js
@@ -2,12 +2,10 @@ $.getJSON("default.json", function (data) {
   $.each(data, function (key, data) {
     $(".new-table").append(
       `<tr>
-        <th>${data.id}</th>
-        <th>${data.balance}</th>
         <th>${data.name}</th>
+        <th>${data.balance}</th>       
         <th>${data.email}</th>
       </tr>`
     );
   });
 });
-
