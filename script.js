f68a3a90e7e986f5920cdefef93473d3d14f4114
Никита <nikitostokarev22@gmail.com>
2020-08-24 10:47:55 +0300

    add childrens to table items

--- script.js
+++ script.js
@@ -1,42 +1,47 @@
-$(function(){
-$.getJSON("default.json", function (data) {
-  $.each(data, function (key, data) {
-      if(data.parentId === 0){
-      $(".new-table").append(
-        `<tr id="${data.id}" class="${data.isActive ? "active" : "unactive"}">
+$(function () {
+  $.getJSON("default.json", function (data) {
+    $.each(data, function (key, data) {
+      if (data.parentId === 0) {
+        $(".new-table").append(
+          `<tr id="${data.id}" class="${data.isActive ? "active" : "unactive"}">
         <td>${data.name}</td>
         <td>${data.balance}</td>       
         <td>${data.email}</td>
       </tr>`
-      );
+        );
       } else {
-          console.log(data.parentId)
+        $(`#${data.parentId}`).append( 
+            `<tr id="${data.id}" class="${data.isActive ? "active" : "unactive"}">
+                <td>${data.name}</td>
+                <td>${data.balance}</td>       
+                <td>${data.email}</td>
+            </tr>`);
       }
-  });
+    });
 
-  $('.sort-active').click(function(){
-     $('.unactive').each(function(){
-         $(this).hide();
-     })
-     $('.active').each(function(){
-        $(this).show();
-    })
-  })
-  $('.sort-unactive').click(function(){
-    $('.active').each(function(){
+    $(".sort-active").click(function () {
+      $(".unactive").each(function () {
         $(this).hide();
-    })
-    $('.unactive').each(function(){
+      });
+      $(".active").each(function () {
         $(this).show();
-    })
- })
- $('.unsort').click(function(){
-    $('.active').each(function(){
+      });
+    });
+    $(".sort-unactive").click(function () {
+      $(".active").each(function () {
+        $(this).hide();
+      });
+      $(".unactive").each(function () {
         $(this).show();
-    })
-    $('.unactive').each(function(){
+      });
+    });
+    $(".unsort").click(function () {
+      $(".active").each(function () {
         $(this).show();
-    })
- })
-});
+      });
+      $(".unactive").each(function () {
+        $(this).show();
+      });
+    });
+  });
 });
