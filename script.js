d0c6520d856e7e1bc495021b7e036a1d12600855
Никита <nikitostokarev22@gmail.com>
2020-08-24 10:10:56 +0300

    add sort buttons

--- script.js
+++ script.js
@@ -1,16 +1,42 @@
+$(function(){
 $.getJSON("default.json", function (data) {
   $.each(data, function (key, data) {
-    if (data.parentId === 0) {
+      if(data.parentId === 0){
       $(".new-table").append(
-        `<tr id="${data.id}" class="100">
-        <th>${data.name}</th>
-        <th>${data.balance}</th>       
-        <th>${data.email}</th>
+        `<tr id="${data.id}" class="${data.isActive ? "active" : "unactive"}">
+        <td>${data.name}</td>
+        <td>${data.balance}</td>       
+        <td>${data.email}</td>
       </tr>`
       );
-    }
+      } else {
+          console.log(data.parentId)
+      }
   });
-  $('.100').click( function() {
-    $(this).hide()
+
+  $('.sort-active').click(function(){
+     $('.unactive').each(function(){
+         $(this).hide();
+     })
+     $('.active').each(function(){
+        $(this).show();
+    })
+  })
+  $('.sort-unactive').click(function(){
+    $('.active').each(function(){
+        $(this).hide();
+    })
+    $('.unactive').each(function(){
+        $(this).show();
+    })
+ })
+ $('.unsort').click(function(){
+    $('.active').each(function(){
+        $(this).show();
+    })
+    $('.unactive').each(function(){
+        $(this).show();
+    })
+ })
 });
 });
